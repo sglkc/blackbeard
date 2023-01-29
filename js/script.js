66b51151a11108c9fcc3f769e9230d484da0a15c
sglkc <31957516+sglkc@users.noreply.github.com>
2023-01-29 15:57:30 +0700

    finish about peeeg

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 AOS.init({
-  easing: 'ease-in-sine'
+  offset: 100,
+  anchorPlacement: 'center-center',
 });
 
 new simpleParallax(document.querySelector('#top-bg img:nth-child(1)'), { delay: .15 });
 new simpleParallax(document.querySelector('#top-bg img:nth-child(2)'), { delay: .3 });
 new simpleParallax(document.querySelector('#top-bg img:nth-child(3)'), { delay: .45 });
+new simpleParallax(document.querySelectorAll('img.parallax'), { delay: 1, scale: 2 });
 
 document.querySelector('nav button').addEventListener('click', ({ target }) => {
   target.parentElement.classList.toggle('open');
 });
-
-/*
-document.addEventListener('scroll', function (e) {
-  console.log(e);
-});
-*/
