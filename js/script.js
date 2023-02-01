c86fe2c40d0fa23bd47dd7b6301f8689b0342fd5
sglkc <31957516+sglkc@users.noreply.github.com>
2023-02-01 15:20:49 +0700

    drop the project :(

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ new simpleParallax(document.querySelectorAll('img.parallax'), { delay: 1, scale:
 document.querySelector('nav button').addEventListener('click', ({ target }) => {
   target.parentElement.classList.toggle('open');
 });
+
+const observer = new IntersectionObserver(
+  ([e]) => {
+    document
+      .querySelector(`nav a[href="#${e.target.id}"]`)
+      .classList
+      .toggle('active', e.isIntersecting);
+
+    if (e.target.id === 'top') {
+      document.querySelector('nav').classList.toggle('expand', !e.isIntersecting);
+    }
+  },
+  { rootMargin: '0% 0% -50% 0%' }
+);
+
+document.querySelectorAll('section').forEach((e) => { observer.observe(e) });
+document.querySelector(`nav a[href="${location.hash}"]`)?.classList.add('active');
